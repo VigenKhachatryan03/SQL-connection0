eca78a68f7333b5a8c25c1aa76c57aef8866369c
a <a@DESKTOP-TU47UA5>
2023-01-25 21:53:13 +0400

    Add project files.

---
+++ SQL connection0/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SQL_connection0
+{
+    class Program
+    {
+        static string connection = "SERVER = (localdb)/mssqllocaldb; DATABASE = HomeWork01;";
+         
+        static void Main(string[] args)
+        {
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=HomeWork01;Integrated Security=True";
+            try
+            {
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+                Console.WriteLine("Connection succsesful");
+
+                Console.WriteLine("Enter your ID");
+                int ID = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter your Name");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter your Age");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter your Password");
+                string Pass = Console.ReadLine();
+
+                string insertQuerty = "" +
+                    "insert into Users([ID] , [name], [username], [password])" +
+                    "values(" + ID + ",'" + name+"', "+age+", '"+Pass+"')";
+                SqlCommand insertCommand = new SqlCommand(insertQuerty, sqlConnection);
+                insertCommand.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                
+            }
+
+        }
+    }
+}
